ea5fb7044d39ebfe358c4d34656cf28f0eb02984
vamsi <vamsibattu3015@gmail.com>
2024-11-04 16:52:20 +0530

    commited

---
+++ 20-sg/variables.tf
@@ -0,0 +1,48 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+variable "mysql_sg_tags" {
+    default = {
+        component = "mysql"
+    }
+
+}
+
+variable "backend_sg_tags" {
+    default = {
+        component = "backend"
+    }
+
+}
+
+variable "frontend_sg_tags" {
+    default = {
+        component = "backend"
+    }
+
+}
+
+variable "bastion_sg_tags" {
+    default = {
+        component = "bastion"
+    }
+}
+
+variable "ansible_sg_tags" {
+    default = {
+        component = "ansible"
+    }
+}
